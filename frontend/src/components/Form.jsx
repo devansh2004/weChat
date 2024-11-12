5aec5e4fcdee493b6db6d942aec817d1b0985dd7
Devansh Patel <patel.devansh@outlook.com>
2024-11-12 19:42:31 +0530

    fixed bugs, moved .env to same dir as vite.config.js

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -14,9 +14,11 @@ function Form({route, method}){
         e.preventDefault();
 
         try{
-           
+            
+          
             const res = await api.post(route, {username,password});
             if(method === "login"){
+                
                 localStorage.setItem(ACCESS_TOKEN, res.data.access);
                 localStorage.setItem(REFRESH_TOKEN,res.data.refresh);
                 navigate("/");
